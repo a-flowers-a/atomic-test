d0ab771143caf0dadf715c6565117712318da2fd
alejandro <alejandro@alejandros-MacBook-Pro.local>
2021-07-02 12:36:53 -0500

    bold and underline footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ function Footer(){
     const linkedin = "../../assets/PaqueteAtomic/linkedin.png";
     const twitter = "../../assets/PaqueteAtomic/twitter.png";
     return(
-        <Row className="footer justify-content-center v-center-content">
+        <Row className="footer bold-text justify-content-center v-center-content">
             <Col sm={6}>© 2020 AtomicLabs. Todos los derechos reservados.</Col>
-            <Col sm={2}>Aviso de privacidad</Col>
-            <Col xs={1}><Image alt={'LinkedIn'} className={"social-img"} src={linkedin}/></Col>
-            <Col xs={1}><Image alt={'Twitter'} className={"social-img"} src={twitter}/></Col>
+            <Col sm={2} className="underline pointer">Aviso de privacidad</Col>
+            <Col xs={1}><Image alt={'LinkedIn'} className="social-img" src={linkedin}/></Col>
+            <Col xs={1}><Image alt={'Twitter'} className="social-img" src={twitter}/></Col>
         </Row>
     );
 }//Footer
