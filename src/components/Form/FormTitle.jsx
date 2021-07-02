6f1682571079363876c43847d44e73de14632e62
alejandro <alejandro@alejandros-MacBook-Pro.local>
2021-07-02 15:04:10 -0500

    form title set psoitions responsive

--- src/components/Form/FormTitle.jsx
+++ src/components/Form/FormTitle.jsx
@@ -5,15 +5,15 @@ function FormTitle(props){
     const {title, img} = props;
     const titleWords = title.split(" ");
     return(
-        <Row  className="justify-content-center m-v-space">
-            <Col className="right-text">
+        <Row  className="v-center-content m-v-space">
+            <Col md={1}>
                 <Image 
                     alt="Number"
                     className="m-img"
                     src={img}    
                 />
-                </Col>
-            <Col sm={11} className="left-text"><h1 className="inline white">{(titleWords.slice(0,2)).join(" ")+" "}</h1> <h1 className="inline orange"> {titleWords[2]}</h1></Col>
+            </Col>
+            <Col md={8}><h1 className="inline white">{(titleWords.slice(0,2)).join(" ")+" "}</h1> <h1 className="inline orange"> {titleWords[2]}</h1></Col>
         </Row>
     );
 }
